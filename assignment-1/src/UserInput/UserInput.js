b4fa2bcd558e3cf04fd3d1ba7be6a8c3cfecff46
masonsteeger94 <masonsteeger@gmail.com>
2020-12-10 16:37:28 -0600

    added user input and output componenets

--- assignment-1/src/UserInput/UserInput.js
+++ assignment-1/src/UserInput/UserInput.js
@@ -1,2 +1,11 @@
 import React, { Component } from 'react';
 
+const UserInput = () => {
+    return(
+        <div>
+            <input type='text' />
+        </div>
+    )
+}
+
+export default UserInput;
---
+++ assignment-1/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const UserOutput = () => {
+    return(
+    <div>
+        <p>Lorem</p>
+        <p>Ipsum</p>
+    </div>
+    )
+}
+
+export default UserOutput;
