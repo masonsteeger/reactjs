bdf2a0e5100683b82ef1db51e07ed9027338db97
masonsteeger94 <masonsteeger@gmail.com>
2020-12-21 16:37:40 -0600

    added styled components

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,11 +47,16 @@ class App extends Component {
 
   render() {
   const style = {
-    backgroundColor: 'pink',
+    backgroundColor: 'green',
+    color: 'white',
     font: 'inherit',
     border: '3px solid blue',
     padding: '8px',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: 'lightgreen',
+      color: 'black'
+    }
   }
 
     let persons = null;
@@ -69,16 +75,31 @@ class App extends Component {
             )
           })}
         </div>
-      )
+      );
+      style.backgroundColor = 'red';
+      style[':hover']={
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button style={style} onClick={this.togglePersonHandler}>Toggle View Persons</button>
-       {persons}
-      </div>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonHandler}>Toggle View Persons</button>
+        {persons}
+        </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import './Person.css'
+import styled from 'styled-components';
+import './Person.css';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 2px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    @media (min-width: 600px){
+        width: 450px;
+    }
+    `;
+    
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 600px)' : {
+            width: '450px'
+        }
+    }
     return (
-        <div className='Person'>
+        // <div className='Person' onClick={props.click} style={style}>
+        <StyledDiv>
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <button onClick={props.click}>DELETE</button>
             <input type='text' onChange ={props.changed} value={props.name}/>
-        </div>
+        </StyledDiv>
+        // </div>
         
     )
 }
