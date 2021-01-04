879564642ff98ed105c2a422f647da974538f4b1
masonsteeger94 <masonsteeger@gmail.com>
2021-01-04 17:43:28 -0600

    added in an error boundry component

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -47,43 +47,43 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = [classes.Button];
 
     if(this.state.showPersons){
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return(
-              <Person
+                <Person
                 click = {() => this.deletePersonHandler(index)}
                 name={person.name} 
                 age={person.age}
-                key={person.id} 
                 changed={(event) => this.nameChangeHandler(event, person.id)}/>
             )
           })}
         </div>
       );
-
-
+      
+      btnClass.push(classes.Red)
     }
 
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass.join(' ')} onClick={this.togglePersonHandler}>
             Toggle View Persons
-          </StyledButton>
+          </button>
         {persons}
         </div>
     );
