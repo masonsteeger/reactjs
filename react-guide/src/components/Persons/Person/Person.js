3610d76d79c0bd2d5b389506cdc8a8cafd9dacef
masonsteeger94 <masonsteeger@gmail.com>
2021-01-05 14:20:45 -0600

    restructured code to seperate out components better

--- react-guide/src/components/Persons/Person/Person.js
+++ react-guide/src/components/Persons/Person/Person.js
@@ -4,8 +4,8 @@ import classes from './Person.css';
 
 const person = (props) => {
     return(
-        <div className={classes.Person} onClick = {props.click}>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className={classes.Person}>
+            <p onClick = {props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type='text' onChange ={props.changed} value={props.name}/>
         </div>
