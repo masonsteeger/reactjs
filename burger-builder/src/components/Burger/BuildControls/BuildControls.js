9ef5f888a1eb08b3b83f77280f1886bc497f365d
masonsteeger94 <masonsteeger@gmail.com>
2021-01-06 19:30:16 -0600

    added purchase modal

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -12,12 +12,20 @@ const controls = [
 
 const buildControls = props => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
                 label={ctrl.label}
-                added={() => props.addIngredient(ctrl.type)} />
+                added={() => props.addIngredient(ctrl.type)} 
+                subtracted={() => props.subtractIngredient(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+                />
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.showPurchase}>ORDER NOW</button>
     </div>
 );
 
