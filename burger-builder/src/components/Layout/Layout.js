9065627ca3b5ab03b01bf8f14a0a372abf0f9110
masonsteeger94 <masonsteeger@gmail.com>
2021-01-06 21:59:01 -0600

    added sidebar for mobile

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
-
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = ( props ) => (
-    <Aux>
-        <div>ToolBar, Sidedrawer, Backdrop </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component{
+    state = {
+        sideDrawerShow:true
+    }
 
-export default layout;
+    sideDrawerClose = () => {
+        this.setState({sideDrawerShow:false})
+    }
+
+    sideDrawerOpen = () => {
+        this.setState({sideDrawerShow:true})
+    }
+
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar open={this.sideDrawerOpen} />
+                <SideDrawer 
+                    open={this.state.sideDrawerShow} 
+                    closed={this.sideDrawerClose} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
+
+export default Layout;
