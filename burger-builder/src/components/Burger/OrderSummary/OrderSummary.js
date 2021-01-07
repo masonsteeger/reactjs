0a973b64eb44e891843a03b897ba2a5cd4e7b3bb
masonsteeger94 <masonsteeger@gmail.com>
2021-01-07 17:16:14 -0600

    OrderSummary changed to class, now updates whenever opened instead of whenever ingredient added

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map((igKey) => {
-            return (<li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}</li>)
-        })
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delcious burger with the following ingredients</p>
-            <ul>
-               {ingredientSummary}
-            </ul>
-            <p><strong>Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
+class OrderSummary extends Component {
+    
+    componentDidUpdate() {
+        console.log('[OrderSummary] updated')
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map((igKey) => {
+                return (<li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}</li>)
+            })
+        return(
+                <Aux>
+                    <h3>Your Order</h3>
+                    <p>A delcious burger with the following ingredients</p>
+                    <ul>
+                       {ingredientSummary}
+                    </ul>
+                    <p><strong>Price: ${this.props.price.toFixed(2)}</strong></p>
+                    <p>Continue to Checkout?</p>
+                    <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                    <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+                </Aux>
+
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
