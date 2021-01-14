0be2ff663b43ff0a08252bc7f7177bac84d3c082
masonsteeger94 <masonsteeger@gmail.com>
2021-01-13 19:47:38 -0600

    finished routing assignment

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+    state = {
+        loadedCourse: {title: '', id: ''}
+    }
+
+    componentDidMount(){
+        this.loadData()
+    }
+
+    componentDidUpdate(){
+        this.loadData()
+    }
+
+    loadData() {
+        if (this.props.match.params.id){
+            if(!this.state.loadedCourse || this.state.loadedCourse && this.state.loadedCourse.id != this.props.match.params.id)
+            this.setState({
+                loadedCourse: this.props.location.state
+
+            })
+        }
+    }
+
     render () {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.state.loadedCourse.title}</h1>
+                <p>Course ID: {this.state.loadedCourse.id}</p>
             </div>
         );
     }
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
+import Course from '../Course/Course';
 import './Courses.css';
 
 class Courses extends Component {
@@ -10,18 +11,36 @@ class Courses extends Component {
             { id: 3, title: 'PWA - The Complete Guide' }
         ]
     }
+    
+    courseHandler = (id, title) => {
+        this.props.history.push({
+            pathname: '/courses/' + id,
+            state: 
+                {
+                    title: title,
+                    id: id
+                }
+            })
+    }
 
     render () {
+
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return (
+                                <article className="Course" 
+                                        key={course.id} 
+                                        onClick={() => this.courseHandler(course.id, course.title)}
+                                        >{course.title}</article>
+                            );
                         } )
                     }
                 </section>
+                <Route path={this.props.match.url + '/:id'} exact component={Course} />
             </div>
         );
     }
